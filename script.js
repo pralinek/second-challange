3605357d24b3e281577fa6e4bc9ac9067c73ffac
pralinek <tolech89@gmail.com>
2021-03-14 16:24:04 +0100

    first-commit

---
+++ script.js
@@ -0,0 +1,9 @@
+/*const button = document.querySelector(".button");
+const buttonactive = button.querySelector(".buttonactive");
+
+button.addEventListener("click", ()=>{
+buttonactive.style.opacity = 1;
+setTimeout(()=>{buttonactive.style.opacity =0}, 3000)
+}
+)
+/*
